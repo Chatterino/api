4e33caa9b1037a0d1b54d60276de224134b34e6b
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2018-06-16 12:20:06 +0200

    make the api fetch live data

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -13,29 +15,32 @@ import (
 )
 
 var kvCache *cache.Cache
+var firstRun = true
+
+const offlineMode = false
 
 func getData(url, key string) ([]byte, error) {
-	raw, err := ioutil.ReadFile("./cached/" + key)
+	if offlineMode {
+		raw, err := ioutil.ReadFile("./cached/" + key)
+		if err != nil {
+			return nil, err
+		}
+
+		return raw, nil
+	}
+
+	resp, err := http.Get(url)
+	log.Printf("Fetching %s live...", url)
 	if err != nil {
 		return nil, err
 	}
-
-	return raw, nil
-
-	/*
-		resp, err := http.Get(url)
-		log.Printf("Fetching %s live...", url)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return nil, err
-		}
-		kvCache.Set(key, body, cacheDuration)
-		return body, nil
-	*/
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Fetched!")
+	return body, nil
 }
 
 func cacheRequest(url, key string, cacheDuration time.Duration) func(w http.ResponseWriter, r *http.Request) {
@@ -64,19 +69,53 @@ func cacheRequest(url, key string, cacheDuration time.Duration) func(w http.Resp
 type EmoteSet struct {
 	ChannelName string `json:"channel_name"`
 	ChannelID   string `json:"channel_id"`
+	Type        string `json:"type"`
+	Custom      bool   `json:"custom"`
 }
 
-var emoteSets map[string]EmoteSet
+var emoteSets map[string]*EmoteSet
+var emoteSetMutex sync.Mutex
+
+func addEmoteSet(emoteSetID, channelName, channelID, setType string) {
+	emoteSets[emoteSetID] = &EmoteSet{
+		ChannelName: channelName,
+		ChannelID:   channelID,
+		Type:        setType,
+		Custom:      true,
+	}
+}
 
 func refreshEmoteSetCache() {
+	if firstRun {
+		emoteSetMutex.Lock()
+		defer emoteSetMutex.Unlock()
+	}
+
 	data, err := getData("https://twitchemotes.com/api_cache/v3/sets.json", "twitchemotes:sets")
 	if err != nil {
 		panic(err)
 	}
+
+	if !firstRun {
+		emoteSetMutex.Lock()
+		defer emoteSetMutex.Unlock()
+	}
+
+	firstRun = false
+	emoteSets = make(map[string]*EmoteSet)
+
 	err = json.Unmarshal(data, &emoteSets)
 	if err != nil {
 		panic(err)
 	}
+
+	for k, _ := range emoteSets {
+		emoteSets[k].Type = "sub"
+	}
+
+	addEmoteSet("13985", "evohistorical2015", "129284508", "sub")
+
+	fmt.Println("Refreshed emote sets")
 
 	time.AfterFunc(30*time.Minute, refreshEmoteSetCache)
 }
@@ -84,12 +123,17 @@ func refreshEmoteSetCache() {
 func setHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	setID := vars["setID"]
+	emoteSetMutex.Lock()
+	defer emoteSetMutex.Unlock()
 	data, err := json.Marshal(emoteSets[setID])
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("returning data %s", data)
-	w.Write(data)
+	fmt.Printf("%s: returning data %s\n", setID, data)
+	_, err = w.Write(data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 var host = flag.String("h", ":1234", "host of server")
@@ -97,7 +141,6 @@ var host = flag.String("h", ":1234", "host of server")
 func main() {
 	flag.Parse()
 	go refreshEmoteSetCache()
-	emoteSets = make(map[string]EmoteSet)
 	kvCache = cache.New(30*time.Minute, 10*time.Minute)
 
 	router := mux.NewRouter()
