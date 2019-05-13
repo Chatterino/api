c42a2adbd8cf83a3b818cabb0663bf2b0c4cc475
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-05-13 11:21:03 +0200

    add some more health endpoints

--- health.go
+++ health.go
@@ -3,15 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
+func uptimeString() string {
+	return time.Since(startTime).String()
+}
+
+func memoryString() string {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	return fmt.Sprintf("Alloc=%v MiB, TotalAlloc=%v MiB, Sys=%v MiB, NumGC=%v",
+		m.Alloc/1024/1024,
+		m.TotalAlloc/1024/1024,
+		m.Sys/1024/1024,
+		m.NumGC)
+}
+
 func healthUptime(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%s", time.Since(startTime))
+	fmt.Fprintf(w, "%s", uptimeString())
+}
+
+func healthMemory(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s", memoryString())
+}
+
+func healthCombined(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Uptime: %s - Memory: %s", uptimeString(), memoryString())
 }
 
 func handleHealth(router *mux.Router) {
 	router.HandleFunc("/health/uptime", healthUptime).Methods("GET")
+	router.HandleFunc("/health/memory", healthMemory).Methods("GET")
+	router.HandleFunc("/health/combined", healthCombined).Methods("GET")
 }
