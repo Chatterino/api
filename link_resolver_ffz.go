567cd78f0928e33fdd84d020946a3f3cb31cd75d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-11-07 14:15:17 +0100

    Add example json response error with information about the struct generation

--- link_resolver_ffz.go
+++ link_resolver_ffz.go
@@ -43,6 +43,36 @@ func init() {
 		Uploader string
 	}
 
+	/* Example JSON data generated from https://api.frankerfacez.com/v1/emote/131001 2020-11-07
+	{
+	  "emote": {
+	    "created_at": "Sun, 25 Sep 2016 12:30:30 GMT",
+	    "css": null,
+	    "height": 21,
+	    "hidden": false,
+	    "id": 131001,
+	    "last_updated": "Sun, 25 Sep 2016 14:25:01 GMT",
+	    "margins": null,
+	    "modifier": false,
+	    "name": "pajaE",
+	    "offset": null,
+	    "owner": {
+	      "_id": 63119,
+	      "display_name": "pajaSWA",
+	      "name": "pajaswa"
+	    },
+	    "public": true,
+	    "status": 1,
+	    "urls": {
+	      "1": "//cdn.frankerfacez.com/8542ab940f02f3bdc938796dc7258902.PNG",
+	      "2": "//cdn.frankerfacez.com/c1fe2e20b1d13e97b40b44f6893a7ba4.PNG",
+	      "4": "//cdn.frankerfacez.com/6154d1c0f922ee6506cb2e555dd46e03.png"
+	    },
+	    "usage_count": 9,
+	    "width": 32
+	  }
+	}
+	*/
 	type FrankerFaceZEmoteAPIResponse struct {
 		Height       int16  `json:"height"`
 		Modifier     bool   `json:"modifier"`
