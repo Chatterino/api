253b8fc75878022e48f85ba595791fcb3283428d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-03-21 16:21:13 +0100

    Add a duration humanizer function

--- pkg/humanize/date.go
+++ pkg/humanize/date.go
@@ -1,6 +1,9 @@
 package humanize
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Date converts a date from a string in the RFC3339 format into one specified in the format string
 func Date(format string, str string) string {
@@ -9,4 +12,21 @@ func Date(format string, str string) string {
 		return ""
 	}
 	return date.Format(format)
+}
+
+// Duration takes a `time.Duration` and converts it to the nearest-second string output
+// Example output: 01:59:59
+func Duration(duration time.Duration) string {
+	// Truncate away any non-second data
+	duration = duration.Truncate(1 * time.Second)
+
+	var hours, minutes, seconds time.Duration
+
+	hours = duration / time.Hour
+	duration -= hours * time.Hour
+	minutes = duration / time.Minute
+	duration -= minutes * time.Minute
+	seconds = duration / time.Second
+
+	return fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
 }
