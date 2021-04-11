d766907142e7ffc7c2496d5d4e0632022ee13673
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-04-11 14:06:53 +0200

    Make twitch tooltip a single line for easier testing

--- internal/resolvers/twitch/resolver.go
+++ internal/resolvers/twitch/resolver.go
@@ -16,13 +16,14 @@ import (
 )
 
 const (
-	twitchClipsTooltipString = `<div style="text-align: left;">
-<b>{{.Title}}</b><hr>
-<b>Clipped by:</b> {{.AuthorName}}<br>
-<b>Channel:</b> {{.ChannelName}}<br>
-<b>Duration:</b> {{.Duration}}<br>
-<b>Created:</b> {{.CreationDate}}<br>
-<b>Views:</b> {{.Views}}</div>`
+	twitchClipsTooltipString = `<div style="text-align: left;">` +
+		`<b>{{.Title}}</b><hr>` +
+		`<b>Clipped by:</b> {{.AuthorName}}<br>` +
+		`<b>Channel:</b> {{.ChannelName}}<br>` +
+		`<b>Duration:</b> {{.Duration}}<br>` +
+		`<b>Created:</b> {{.CreationDate}}<br>` +
+		`<b>Views:</b> {{.Views}}` +
+		`</div>`
 )
 
 var (
