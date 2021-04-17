12ac2989b4385451eec07f854f4551ad29ed524f
Karar Al-Remahy <alremahykarar@ymail.com>
2021-04-17 13:50:13 +0200

    fix import & error capitalization

--- internal/resolvers/default/thumbnail_windows.go
+++ internal/resolvers/default/thumbnail_windows.go
@@ -3,10 +3,11 @@
 package defaultresolver
 
 import (
+	"errors"
 	"net/http"
 )
 
 func buildThumbnailByteArray(inputBuf []byte, resp *http.Response) ([]byte, error) {
 	// Since the lilliput library currently does not support Windows, we error out early and fall back to the static thumbnail generation
-	return nil, errors.New("Cannot build animated thumbnails on Windows")
+	return nil, errors.New("cannot build animated thumbnails on windows")
 }
