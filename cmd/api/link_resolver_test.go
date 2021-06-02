5465b14f2e5a205b36bd6a8c0bc34fe4cbec3cf3
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-03 11:38:18 +1200

    Create tests for YouTube channel link resolution

--- cmd/api/link_resolver_test.go
+++ cmd/api/link_resolver_test.go
@@ -66,6 +66,87 @@ func TestResolveTwitchClip2(t *testing.T) {
 	}
 }
 
+func TestResolveYouTubeChannelUserStandard(t *testing.T) {
+	router := chi.NewRouter()
+	defaultresolver.Initialize(router, "")
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+	fmt.Println(ts.URL)
+	const url = `https%3A%2F%2Fwww.youtube.com%2Fuser%2Fpenguinz0`
+	res, err := http.Get(ts.URL + "/link_resolver/" + url)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+	var jsonResponse resolver.Response
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		panic(err)
+	}
+	if jsonResponse.Status != 200 {
+		t.Fatal("wrong status from api")
+	}
+}
+
+func TestResolveYouTubeChannelUserShortened(t *testing.T) {
+	router := chi.NewRouter()
+	defaultresolver.Initialize(router, "")
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+	fmt.Println(ts.URL)
+	const url = `https%3A%2F%2Fwww.youtube.com%2Fc%2FMizkifDaily`
+	res, err := http.Get(ts.URL + "/link_resolver/" + url)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+	var jsonResponse resolver.Response
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		panic(err)
+	}
+	if jsonResponse.Status != 200 {
+		t.Fatal("wrong status from api")
+	}
+}
+
+func TestResolveYouTubeChannelIdentifier(t *testing.T) {
+	router := chi.NewRouter()
+	defaultresolver.Initialize(router, "")
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+	fmt.Println(ts.URL)
+	const url = `https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCoqDr5RdFOlomTQI2tkaDOA`
+	res, err := http.Get(ts.URL + "/link_resolver/" + url)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+	var jsonResponse resolver.Response
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		panic(err)
+	}
+	if jsonResponse.Status != 200 {
+		t.Fatal("wrong status from api")
+	}
+}
+
 func TestResolve1M(t *testing.T) {
 	// var resp *http.Response
 	// var err error
