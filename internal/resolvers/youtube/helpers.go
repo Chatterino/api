0cd7be50dd2391e5f534145ce2c40626c70b22b1
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-02 20:20:16 +1200

    Create YouTube channel name extractor utility

--- internal/resolvers/youtube/helpers.go
+++ internal/resolvers/youtube/helpers.go
@@ -17,3 +17,20 @@ func getYoutubeVideoIDFromURL(url *url.URL) string {
 func getYoutubeVideoIDFromURL2(url *url.URL) string {
 	return path.Base(url.Path)
 }
+
+func getYoutubeChannelIdFromURL(url *url.URL) string {
+	segments := strings.Split(url.Path, "/")
+
+	// Get the segment of the path after the channel or user segment
+	for i, segment := range segments {
+		if i == len(segment) - 1 {
+			break
+		}
+
+		if segment == "channel" || segment == "user" {
+			return segments[i + 1]
+		}
+	}
+
+	return ""
+}
