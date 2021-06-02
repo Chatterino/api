5b16c7c114bc719bfebe2bd32d9228220594d177
James Upjohn <jammehcow@jammehcow.co.nz>
2021-06-02 20:22:19 +1200

    Create loader for YouTube channels

--- internal/resolvers/youtube/load.go
+++ internal/resolvers/youtube/load.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/Chatterino/api/pkg/cache"
@@ -13,7 +14,7 @@ import (
 	"github.com/Chatterino/api/pkg/resolver"
 )
 
-func load(videoID string, r *http.Request) (interface{}, time.Duration, error) {
+func loadVideos(videoID string, r *http.Request) (interface{}, time.Duration, error) {
 	youtubeVideoParts := []string{
 		"statistics",
 		"snippet",
@@ -60,6 +61,68 @@ func load(videoID string, r *http.Request) (interface{}, time.Duration, error) {
 	thumbnail := video.Snippet.Thumbnails.Default.Url
 	if video.Snippet.Thumbnails.Medium != nil {
 		thumbnail = video.Snippet.Thumbnails.Medium.Url
+	}
+
+	return &resolver.Response{
+		Status:    http.StatusOK,
+		Tooltip:   url.PathEscape(tooltip.String()),
+		Thumbnail: thumbnail,
+	}, cache.NoSpecialDur, nil
+}
+
+func loadChannels(channelID string, r *http.Request) (interface{}, time.Duration, error) {
+	youtubeChannelParts := []string{
+		"statistics",
+		"snippet",
+	}
+
+	log.Println("[YouTube] GET", channelID)
+	builtRequest := youtubeClient.Channels.List(youtubeChannelParts)
+
+	if strings.HasPrefix(channelID, "UC") {
+		builtRequest = builtRequest.Id(channelID)
+	} else {
+		builtRequest = builtRequest.ForUsername(channelID)
+	}
+
+	youtubeResponse, err := builtRequest.Do()
+
+	if err != nil {
+		return &resolver.Response{
+			Status:  500,
+			Message: "youtube api error " + resolver.CleanResponse(err.Error()),
+		}, 1 * time.Hour, nil
+	}
+
+	if len(youtubeResponse.Items) != 1 {
+		return nil, cache.NoSpecialDur, errors.New("channel response is not size 1")
+	}
+
+	channel := youtubeResponse.Items[0]
+
+	if channel.ContentDetails == nil {
+		return &resolver.Response{Status: 500, Message: "channel unavailable"}, cache.NoSpecialDur, nil
+	}
+
+	data := youtubeChannelTooltipData{
+		Title:        channel.Snippet.Title,
+		PublishDate:  humanize.CreationDateRFC3339(channel.Snippet.PublishedAt),
+		Views:        humanize.Number(channel.Statistics.ViewCount),
+		Description:  channel.Snippet.Description,
+		Subscribers:  humanize.Number(channel.Statistics.SubscriberCount),
+	}
+
+	var tooltip bytes.Buffer
+	if err := youtubeChannelTooltipTemplate.Execute(&tooltip, data); err != nil {
+		return &resolver.Response{
+			Status:  http.StatusInternalServerError,
+			Message: "youtube template error " + resolver.CleanResponse(err.Error()),
+		}, cache.NoSpecialDur, nil
+	}
+
+	thumbnail := channel.Snippet.Thumbnails.Default.Url
+	if channel.Snippet.Thumbnails.Medium != nil {
+		thumbnail = channel.Snippet.Thumbnails.Medium.Url
 	}
 
 	return &resolver.Response{
