83d0083ab3ced00a855e36d91a3d5d97d2e91d7b
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-06-12 15:49:39 +0200

    Various changes
     - Read default values from pflag instead of defining them in a strct
     - Read config files one by one and merge them in as other Linux services do
     - Change config file and flags to use dashes instead of underscores

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,10 @@
 package config
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"os"
-	"reflect"
+	"strings"
 
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
@@ -17,66 +17,83 @@ const (
 
 var (
 	Cfg APIConfig
-	v   = viper.New()
 )
 
-func init() {
-	// Default config
-	ref := reflect.ValueOf(defaultConf)
+// readFromPath reads the config values from the given path (i.e. path/${appName}.yaml) and returns its values as a map
+// This allows us to use the viper.MergeConfigMap to read config values in the unix standard,
+// so you start furthest down with reading the system config file, merge those values into the main config map,
+// then read the home directory config files, and merge any set values from there,
+// and lastly the config file in the cwd and merge those in. If a value is not set in the cwd config file,
+// but one is set in the system config file then the system config file value will be used
+func readFromPath(path string) (values map[string]interface{}, err error) {
+	v := viper.New()
+	v.SetConfigName(appName)
+	v.SetConfigType("yaml")
+	v.AddConfigPath(path)
 
-	// TODO: Figure out a better way to set default values - this will not work with nested keys
-	for i := 0; i < ref.NumField(); i++ {
-		v.SetDefault(ref.Type().Field(i).Tag.Get("mapstructure"), ref.Field(i).Interface())
+	if err = v.ReadInConfig(); err != nil {
+		notFoundError := &viper.ConfigFileNotFoundError{}
+		if errors.As(err, notFoundError) {
+			err = nil
+			return
+		}
+		return
 	}
 
-	// Flags
-	pflag.StringP("base_url", "b", "", "Address to which API will bind and start listening")
-	pflag.StringP("bind_address", "l", "", "Base URL (useful if being proxied through something like nginx). Value needs to be full URL up to the application (e.g. https://braize.pajlada.com/chatterino)")
-	pflag.Uint64("max_content_length", 5*1024*1024, "Max content size (in bytes) - requests with body bigger than this value will be skipped")
-	pflag.Bool("enable_lilliput", true, "When enabled, will attempt to use lilliput library for building animated thumbnails. Could increase memory usage by a lot.")
-	pflag.String("discord_token", "", "Discord token")
-	pflag.String("twitch_client_id", "", "Twitch client ID")
-	pflag.String("twitch_client_secret", "", "Twitch client secret")
-	pflag.String("youtube_api_key", "", "YouTube API key")
-	pflag.String("twitter_bearer_token", "", "Twitter bearer token")
-	pflag.String("imgur_client_id", "", "Imgur client ID")
-	pflag.String("oembed_facebook_app_id", "", "oEmbed Facebook app ID")
-	pflag.String("oembed_facebook_app_secret", "", "oEmbed Facebook app secret")
-	pflag.String("oembed_providers_path", "./providers.json", "Path to a json file containing supported oEmbed resolvers")
+	v.Unmarshal(&values)
+
+	return
+}
+
+func init() {
+	v := viper.New()
+
+	// Define command-line flags and default values
+	pflag.StringP("base-url", "b", "", "Address to which API will bind and start listening")
+	pflag.StringP("bind-address", "l", ":1234", "Base URL (useful if being proxied through something like nginx). Value needs to be full URL up to the application (e.g. https://braize.pajlada.com/chatterino)")
+	pflag.Uint64("max-content-length", 5*1024*1024, "Max content size (in bytes) - requests with body bigger than this value will be skipped")
+	pflag.Bool("enable-lilliput", true, "When enabled, will attempt to use lilliput library for building animated thumbnails. Could increase memory usage by a lot.")
+	pflag.String("discord-token", "", "Discord token")
+	pflag.String("twitch-client-id", "", "Twitch client ID")
+	pflag.String("twitch-client-secret", "", "Twitch client secret")
+	pflag.String("youtube-api-key", "", "YouTube API key")
+	pflag.String("twitter-bearer-token", "", "Twitter bearer token")
+	pflag.String("imgur-client-id", "", "Imgur client ID")
+	pflag.String("oembed-facebook-app-id", "", "oEmbed Facebook app ID")
+	pflag.String("oembed-facebook-app-secret", "", "oEmbed Facebook app secret")
+	pflag.String("oembed-providers-path", "./providers.json", "Path to a json file containing supported oEmbed resolvers")
 	pflag.Parse()
 	v.BindPFlags(pflag.CommandLine)
 
 	// figure out XDG_DATA_CONFIG to be compliant with the standard
 	xdgConfigHome, exists := os.LookupEnv("XDG_CONFIG_HOME")
 	if !exists || xdgConfigHome == "" {
-		xdgConfigHome = fmt.Sprintf("$HOME/.config/%s/", appName)
+		xdgConfigHome = fmt.Sprintf("$HOME/.config/%s", appName)
 	}
 
-	// File
-	v.SetConfigName(appName)
-	v.SetConfigType("yaml")
-	v.AddConfigPath("/etc/")
-	v.AddConfigPath(xdgConfigHome)
-	v.AddConfigPath(".")
+	// config paths to read from, in order of least importance
+	configPaths := []string{
+		"/etc",
+		xdgConfigHome,
+		".",
+	}
 
-	if err := v.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			log.Println("Config file not found")
+	for _, configPath := range configPaths {
+		if configMap, err := readFromPath(configPath); err != nil {
+			fmt.Printf("Error reading config file from %s/%s.yaml: %s\n", configPath, appName, err)
+			return
 		} else {
-			log.Println("Fatal error encountered while reading config file")
-			panic(err)
+			v.MergeConfigMap(configMap)
 		}
-	} else {
-		v.MergeInConfig()
 	}
 
 	// Environment
 	//V.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // will be useful once we have nested keys
+	v.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	v.SetEnvPrefix(envPrefix)
 	v.AutomaticEnv()
 
 	// Print config
-	Cfg = defaultConf
 	v.UnmarshalExact(&Cfg)
 
 	fmt.Printf("%# v\n", Cfg)
--- pkg/config/model.go
+++ pkg/config/model.go
@@ -3,20 +3,20 @@ package config
 type APIConfig struct {
 	// Core
 
-	BaseURL          string `mapstructure:"base_url" json:"base_url"`
-	BindAddress      string `mapstructure:"bind_address" json:"bind_address"`
-	MaxContentLength uint64 `mapstructure:"max_content_length" json:"max_content_length"`
-	EnableLilliput   bool   `mapstructure:"enable_lilliput" json:"enable_lilliput"`
+	BaseURL          string `mapstructure:"base-url" json:"base-url"`
+	BindAddress      string `mapstructure:"bind-address" json:"bind-address"`
+	MaxContentLength uint64 `mapstructure:"max-content-length" json:"max-content-length"`
+	EnableLilliput   bool   `mapstructure:"enable-lilliput" json:"enable-lilliput"`
 
 	// Secrets
 
-	DiscordToken            string `mapstructure:"discord_token" json:"discord_token"`
-	TwitchClientID          string `mapstructure:"twitch_client_id" json:"twitch_client_id"`
-	TwitchClientSecret      string `mapstructure:"twitch_client_secret" json:"twitch_client_secret"`
-	YoutubeApiKey           string `mapstructure:"youtube_api_key" json:"youtube_api_key"`
-	TwitterBearerToken      string `mapstructure:"twitter_bearer_token" json:"twitter_bearer_token"`
-	ImgurClientID           string `mapstructure:"imgur_client_id" json:"imgur_client_id"`
-	OembedFacebookAppID     string `mapstructure:"oembed_facebook_app_id" json:"oembed_facebook_app_id"`
-	OembedFacebookAppSecret string `mapstructure:"oembed_facebook_app_secret" json:"oembed_facebook_app_secret"`
-	OembedProvidersPath     string `mapstructure:"oembed_providers_path" json:"oembed_providers_path"`
+	DiscordToken            string `mapstructure:"discord-token" json:"discord-token"`
+	TwitchClientID          string `mapstructure:"twitch-client-id" json:"twitch-client-id"`
+	TwitchClientSecret      string `mapstructure:"twitch-client-secret" json:"twitch-client-secret"`
+	YoutubeApiKey           string `mapstructure:"youtube-api-key" json:"youtube-api-key"`
+	TwitterBearerToken      string `mapstructure:"twitter-bearer-token" json:"twitter-bearer-token"`
+	ImgurClientID           string `mapstructure:"imgur-client-id" json:"imgur-client-id"`
+	OembedFacebookAppID     string `mapstructure:"oembed-facebook-app-id" json:"oembed-facebook-app-id"`
+	OembedFacebookAppSecret string `mapstructure:"oembed-facebook-app-secret" json:"oembed-facebook-app-secret"`
+	OembedProvidersPath     string `mapstructure:"oembed-providers-path" json:"oembed-providers-path"`
 }
