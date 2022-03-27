fe57a157f3c62b25c49513d9cc902f5f580e179e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-03-27 13:40:52 +0200

    Add tests for resolvers without any tests

---
+++ internal/resolvers/supinic/initialize_test.go
@@ -0,0 +1,29 @@
+package supinic
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Chatterino/api/internal/logger"
+	"github.com/Chatterino/api/pkg/config"
+	"github.com/Chatterino/api/pkg/resolver"
+	"github.com/pashagolub/pgxmock"
+
+	qt "github.com/frankban/quicktest"
+)
+
+func TestInitialize(t *testing.T) {
+	ctx := logger.OnContext(context.Background(), logger.NewTest())
+	c := qt.New(t)
+
+	pool, err := pgxmock.NewPool()
+	c.Assert(err, qt.IsNil)
+
+	c.Run("No credentials", func(c *qt.C) {
+		cfg := config.APIConfig{}
+		customResolvers := []resolver.Resolver{}
+		c.Assert(customResolvers, qt.HasLen, 0)
+		Initialize(ctx, cfg, pool, &customResolvers)
+		c.Assert(customResolvers, qt.HasLen, 1)
+	})
+}
