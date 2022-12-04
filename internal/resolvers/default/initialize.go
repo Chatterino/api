6cccba47d9205c644dfb92a810cdbd08025018ba
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-12-04 13:01:21 +0100

    Remove commented out code

--- internal/resolvers/default/initialize.go
+++ internal/resolvers/default/initialize.go
@@ -28,7 +28,6 @@ var defaultTooltip = template.Must(template.New("default_tooltip").Parse(default
 func Initialize(ctx context.Context, cfg config.APIConfig, pool db.Pool, router *chi.Mux, helixClient *helix.Client) {
 	defaultLinkResolver := New(ctx, cfg, pool, helixClient)
 
-	// cached := stampede.Handler(512, 10*time.Second)
 	imageCached := stampede.Handler(256, 2*time.Second)
 	generatedValuesCached := stampede.Handler(256, 2*time.Second)
 
