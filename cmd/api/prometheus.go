79812ae16c68d58659d6a64f9084fb2d0f436b79
M4tthewDE <mattider2@gmail.com>
2023-01-28 15:26:57 +0100

    Remove unused prometheus metric definition

--- cmd/api/prometheus.go
+++ cmd/api/prometheus.go
@@ -5,17 +5,7 @@ import (
 
 	"github.com/Chatterino/api/pkg/config"
 	"github.com/go-chi/chi/v5"
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
-
-var (
-	servedRoutes = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "requests_total",
-		Help: "Total number of requests",
-	},
-		[]string{"path"},
-	)
 )
 
 func listenPrometheus(cfg config.APIConfig) {
